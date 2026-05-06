4e8bc240258ac4af67eca7797225094f7602cf73
Human-Gechi <kimberlydaniel363@gmail.com>
2026-05-06 14:27:51 +0100

    bigquery test

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -1,11 +1,15 @@
-import pytest
 from unittest.mock import MagicMock, patch
+
+import pytest
+
 from src.connector.bigquery_connector import BigQueryConn
+
 
 @pytest.fixture
 def bigquery_conn():
     conn = BigQueryConn(project="test-project", credentials_path="fake.json")
     return conn
+
 
 @patch("src.connector.bigquery_connector.service_account.Credentials.from_service_account_file")
 @patch("src.connector.bigquery_connector.bigquery.Client")
@@ -19,14 +23,14 @@ def test__enter_and__exit(mock_client, mock_creds, bigquery_conn):
         assert hasattr(bigquery_conn, "conn")
     mock_client_instance.close.assert_called_once()
 
+
 @patch("src.connector.bigquery_connector.service_account.Credentials.from_service_account_file")
 @patch("src.connector.bigquery_connector.bigquery.Client")
 def test_get_dataset_location(mock_client, mock_creds, bigquery_conn):
     mock_creds.return_value = MagicMock()
     mock_client_instance = MagicMock()
-    mock_client_instance.project = 'test-project'
+    mock_client_instance.project = "test-project"
     mock_client.return_value = mock_client_instance
-
 
     mock_dataset = MagicMock()
     mock_dataset.location = "US"

0491ada5fbc8cbf5ad07d1c8ba9f3062bc14d187
Human-Gechi <kimberlydaniel363@gmail.com>
2026-05-06 16:36:29 +0100

    test

--- tests/test_connectors.py
+++ tests/test_connectors.py
@@ -40,3 +40,47 @@ def test_get_dataset_location(mock_client, mock_creds, bigquery_conn):
         location = bigquery_conn.get_dataset_location(client, "my_dataset")
         assert location == "US"
         mock_client_instance.get_dataset.assert_called_once_with("test-project.my_dataset")
+
+
+@patch("src.connector.bigquery_connector.service_account.Credentials.from_service_account_file")
+@patch("src.connector.bigquery_connector.bigquery.Client")
+def test_dataset_exists(mock_client, mock_creds, bigquery_conn):
+    mock_creds.return_value = MagicMock()
+    mock_client_instance = MagicMock()
+    mock_client_instance.project = "test-project"
+    mock_client.return_value = mock_client_instance
+
+    mock_client_instance.get_dataset.return_value = True
+
+    with bigquery_conn as client:
+        bigquery_conn.dataset_exists(client, "my_dataset")
+        mock_client_instance.get_dataset.assert_called_once_with("test-project.my_dataset")
+
+
+@patch("src.connector.bigquery_connector.service_account.Credentials.from_service_account_file")
+@patch("src.connector.bigquery_connector.bigquery.Client")
+def test_dataset_not_exists(mock_client, mock_creds, bigquery_conn):
+    mock_creds.return_value = MagicMock()
+    mock_client_instance = MagicMock()
+    mock_client_instance.project = "test-project"
+    mock_client.return_value = mock_client_instance
+    mock_client_instance.get_dataset.side_effect = Exception(False)
+
+    with bigquery_conn as client:
+        result = bigquery_conn.dataset_exists(client, "my_dataset")
+        assert result is False
+        mock_client_instance.get_dataset.assert_called_once_with("test-project.my_dataset")
+
+
+@patch("src.connector.bigquery_connector.service_account.Credentials.from_service_account_file")
+@patch("src.connector.bigquery_connector.bigquery.Client")
+def test_table_exists(mock_client, mock_creds, bigquery_conn):
+    mock_creds.return_value = MagicMock()
+    mock_client_instance = MagicMock()
+    mock_client_instance.project = "test-project"
+    mock_client.return_value = mock_client_instance
+
+    with bigquery_conn as client:
+        result = bigquery_conn.table_exists(client, "my_dataset", "my_table")
+        assert result is True
+        mock_client_instance.get_table.assert_called_once_with("test-project.my_dataset.my_table")
